ce8555b2e604e90322d07f97d04ba6c00d839b24
Stanislav Bolshakov <st.bolshakov@gmail.com>
2023-07-20 12:50:10 +0300

    ci(pesic): GitHub CI

---
+++ bump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
+
+sed -i "s/version=\"[^']*\"/version=\"$1\"/" setup.py
+echo "Version updated to $1 in setup.py"

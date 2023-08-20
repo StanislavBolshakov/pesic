788279423cf484c4b958046837ccd2c9afcd15fd
Stanislav Bolshakov <st.bolshakov@gmail.com>
2023-08-20 20:03:43 +0300

    feat(pesic): adjust for /v6/ api endpoint

--- pesic/custom_exceptions.py
+++ pesic/custom_exceptions.py
@@ -2,11 +2,9 @@ class MissingArgumentError(ValueError):
     def __init__(self, msg: str = None):
         self.msg = msg
 
-
 class UnsupportedArgumentError(ValueError):
     def __init__(self, msg: str = None):
         self.msg = msg
-
 
 class UnsupportedMeterType(ValueError):
     def __init__(self, msg: str = None):
